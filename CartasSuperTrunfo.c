82244184ba8f292af646e1574e3716ae2f265a10
RaeldaCruzSilva <raeldacruzsilva@gmail.com>
2025-03-18 01:34:20 +0000

    inserindo comentários

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,7 +18,7 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
     //Carta 1
-
+    //Iniciamos o código com a declaração das variáveis, definindo o tipo de cada uma.
     char estado;
     char codigo_cidade[10];
     char nome_cidade[15];
@@ -28,7 +28,8 @@ int main() {
     int ponto_turistico;
 
     printf("CADASTRO CARTA 01\n");
-    
+    //nesta sessão, são capturadas as infomrações das variáveis, através da função scanf indicando os especificadores de formatos.
+    //utilizamos afunção printf para especificar quais as informações são requeridas para cada variável.
     printf("Estado:");
     scanf("%c", &estado);
 
@@ -51,7 +52,7 @@ int main() {
     scanf("%d", &ponto_turistico);
 
     printf("CARTA 01\n");
-
+    //nesta seção, são impressas as informações coletadas, através da função printf.
     printf(" Estado: %c\n Codigo da Cidade: %s\n Nome da Cidade: %s\n População: %d\n", estado, codigo_cidade, nome_cidade, populacao);
     printf(" Área da Cidade: %.2f\n Pib da Cidade: %.2f\n Número de Pontos Turísticos: %d\n", area_cidade, pib, ponto_turistico);
     
