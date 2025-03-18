6082417a8691321d698c970e5ceb5e9b6a682f4e
RaeldaCruzSilva <raeldacruzsilva@gmail.com>
2025-03-18 01:12:25 +0000

    Correção de erros01

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,6 +17,70 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    //Carta 1
 
+    char estado;
+    char codigo_cidade[10];
+    char nome_cidade[15];
+    int populacao;
+    float area_cidade;
+    float pib;
+    int ponto_turistico;
+
+    printf("CADASTRO CARTA 01\n");
+    
+    printf("Estado:");
+    scanf("%c", &estado);
+
+    printf("Código da Cidade: ");
+    scanf("%s", codigo_cidade);
+
+    printf("Nome da Cidade: ");
+    scanf("%s", nome_cidade);
+
+    printf("População: ");
+    scanf("%d", &populacao);
+
+    printf("Digita Área da Cidade: ");
+    scanf("%f", &area_cidade);
+
+    printf("Pib da Cidade: ");
+    scanf("%f", &pib);
+
+    printf("Número de Pontos Turísticos: ");
+    scanf("%d", &ponto_turistico);
+
+    printf("CARTA 01\n");
+
+    printf(" Estado: %c\n Codigo da Cidade: %s\n Nome da Cidade: %s\n População: %d\n", estado, codigo_cidade, nome_cidade, populacao);
+    printf(" Área da Cidade: %.2f\n Pib da Cidade: %.2f\n Número de Pontos Turísticos: %d\n", area_cidade, pib, ponto_turistico);
+    
+    printf("CADASTRO CARTA 02\n");
+    
+    printf("Estado:");
+    scanf(" %c", &estado);
+
+    printf("Código da Cidade: ");
+    scanf("%s", codigo_cidade);
+
+    printf("Nome da Cidade: ");
+    scanf("%s", nome_cidade);
+
+    printf("População: ");
+    scanf("%d", &populacao);
+
+    printf("Digita Área da Cidade: ");
+    scanf("%f", &area_cidade);
+
+    printf("Pib da Cidade: ");
+    scanf("%f", &pib);
+
+    printf("Número de Pontos Turísticos: ");
+    scanf("%d", &ponto_turistico);
+
+    printf("CARTA 02\n");
+
+    printf(" Estado: %c\n Codigo da Cidade: %s\n Nome da Cidade: %s\n População: %d\n", estado, codigo_cidade, nome_cidade, populacao);
+    printf(" Área da Cidade: %.2f\n Pib da Cidade: %.2f\n Número de Pontos Turísticos: %d\n", area_cidade, pib, ponto_turistico);
     return 0;
 }
