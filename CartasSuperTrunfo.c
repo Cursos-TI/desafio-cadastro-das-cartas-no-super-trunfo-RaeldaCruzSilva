6c6d3f42f2144edf8d0d162780c9750e72e58a56
RaeldaCruzSilva <raeldacruzsilva@gmail.com>
2025-03-21 01:37:56 +0000

    nivel_aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -26,10 +26,14 @@ int main() {
     float area_cidade;
     float pib;
     int ponto_turistico;
+    float dens_populacional;
+    float pib_per_capita;
 
     printf("CADASTRO CARTA 01\n");
+    
     //nesta sessão, são capturadas as infomrações das variáveis, através da função scanf indicando os especificadores de formatos.
     //utilizamos afunção printf para especificar quais as informações são requeridas para cada variável.
+    
     printf("Estado:");
     scanf("%c", &estado);
 
@@ -50,17 +54,22 @@ int main() {
 
     printf("Número de Pontos Turísticos: ");
     scanf("%d", &ponto_turistico);
+    //cálculo da densidade populacional, com a divisão do número de habitantes pela área da cidade
+    dens_populacional = populacao / area_cidade;
 
+    pib_per_capita = pib / populacao;
+    
     printf("CARTA 01\n");
     //nesta seção, são impressas as informações coletadas, através da função printf.
-    printf(" Estado: %c\n Codigo da Cidade: %s\n Nome da Cidade: %s\n População: %d\n", estado, codigo_cidade, nome_cidade, populacao);
-    printf(" Área da Cidade: %.2f\n Pib da Cidade: %.2f\n Número de Pontos Turísticos: %d\n", area_cidade, pib, ponto_turistico);
-    
+    printf(" Estado: %c\n Codigo da Cidade: %s\n Nome da Cidade: %s\n População: %d habitantes\n", estado, codigo_cidade, nome_cidade, populacao);
+    printf(" Área da Cidade: %.2f km²\n Pib da Cidade: R$ %.2f Reais\n Número de Pontos Turísticos: %d\n", area_cidade, pib, ponto_turistico);
+    printf(" Densidade Populacional: %.2f\n", dens_populacional);
+    printf(" Pib per Capita: %.2f\n", pib_per_capita);
+
     printf("CADASTRO CARTA 02\n");
     
     printf("Estado:");
     scanf(" %c", &estado);
-
     printf("Código da Cidade: ");
     scanf("%s", codigo_cidade);
 
@@ -79,9 +88,15 @@ int main() {
     printf("Número de Pontos Turísticos: ");
     scanf("%d", &ponto_turistico);
 
+    dens_populacional = populacao / area_cidade;
+    pib_per_capita = pib / populacao;
+
     printf("CARTA 02\n");
 
-    printf(" Estado: %c\n Codigo da Cidade: %s\n Nome da Cidade: %s\n População: %d\n", estado, codigo_cidade, nome_cidade, populacao);
-    printf(" Área da Cidade: %.2f\n Pib da Cidade: %.2f\n Número de Pontos Turísticos: %d\n", area_cidade, pib, ponto_turistico);
+    printf(" Estado: %c\n Codigo da Cidade: %s\n Nome da Cidade: %s\n População: %d Habitantes\n", estado, codigo_cidade, nome_cidade, populacao);
+    printf(" Área da Cidade: %.2f km²\n Pib da Cidade: R$ %.2f Reais\n Número de Pontos Turísticos: %d\n", area_cidade, pib, ponto_turistico);
+    printf(" Densidade Populacional: %.2f\n", dens_populacional);
+    printf(" Pib per Capita: %.2f\n", pib_per_capita);
+    
     return 0;
 }
